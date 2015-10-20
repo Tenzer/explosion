7a30ca70d266e8d959223ef67919f4ecba643cc5
Jeppe Toustrup <jeppe@tenzer.dk>
2015-10-20 22:52:41 +0200

    Use RGB values from 0-255, that works better in Konsole

--- explosion.go
+++ explosion.go
@@ -24,7 +24,15 @@ func PrintImage(img image.Image) {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			upper_red, upper_green, upper_blue, _ := resized.At(x, y).RGBA()
 			lower_red, lower_green, lower_blue, _ := resized.At(x, y+1).RGBA()
-			fmt.Printf("\x1b[48;2;%d;%d;%dm\x1b[38;2;%d;%d;%dm▄", upper_red, upper_green, upper_blue, lower_red, lower_green, lower_blue)
+			fmt.Printf(
+				"\x1b[48;2;%d;%d;%dm\x1b[38;2;%d;%d;%dm▄",
+				upper_red/256,
+				upper_green/256,
+				upper_blue/256,
+				lower_red/256,
+				lower_green/256,
+				lower_blue/256,
+			)
 		}
 		fmt.Println("\x1b[0m")
 	}
